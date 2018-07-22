f6f5e033e4631e5074d2fde0094e371f57460c55
Tamer Nassar <tamerna@post.bgu.ac.il>
2018-07-22 23:36:34 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -308,14 +308,3 @@ void add_bolod(int score,char* path,LinkedList* l) {
 	insert(l,initLLN(score,path));
 }
 
-string ws2s(wstring& s)
-{
-	int len;
-	int slength = (int)s.length() + 1;
-	len = WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, 0, 0, 0, 0);
-	char* buf = new char[len];
-	WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, buf, len, 0, 0);
-	std::string r(buf);
-	delete[] buf;
-	return r;
-}
